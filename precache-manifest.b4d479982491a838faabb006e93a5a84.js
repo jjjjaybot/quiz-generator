6f2e22f174880b5a67e509b3e4bd1fa1c73f0b85
Jiazi Cai <caijiazi777@gmail.com>
2020-04-28 20:49:45 -0400

    Updates

---
+++ precache-manifest.b4d479982491a838faabb006e93a5a84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29687a5c96c341aa7fe7f913d468a8e9",
+    "url": "/quiz-generator/index.html"
+  },
+  {
+    "revision": "283158db185369e55e66",
+    "url": "/quiz-generator/static/css/main.73959355.chunk.css"
+  },
+  {
+    "revision": "83f078968bb512f4fb59",
+    "url": "/quiz-generator/static/js/2.ee69cf39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-generator/static/js/2.ee69cf39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283158db185369e55e66",
+    "url": "/quiz-generator/static/js/main.d44b91b4.chunk.js"
+  },
+  {
+    "revision": "56657418803130eca919",
+    "url": "/quiz-generator/static/js/runtime-main.fb3486ad.js"
+  }
+]);
